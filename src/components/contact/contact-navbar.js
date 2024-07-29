894c4882c0aeea98d7197fa082ec1b8988ee1c5c
melikeeros <orsmelike2003@gmail.com>
2024-07-29 12:11:57 +0300

    responsive homepage & hamburger menu

--- src/components/contact/contact-navbar.js
+++ src/components/contact/contact-navbar.js
@@ -1,10 +1,17 @@
-import React from "react";
+'use client';
 import Image from 'next/image';
 import upboxLogo from '/public/images/upboxLogo.svg';
 import topbar from '/public/images/topbar.svg';
 import { IoMdMenu } from "react-icons/io";
+import React, {useState} from "react";
 
-export default function ContactNav() {
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar">
       <Image className="navbar__topbar" src={topbar}></Image>
@@ -12,6 +19,9 @@ export default function ContactNav() {
         <div className="navbar__logoGroup">
           <Image className="navbar__logo" src={upboxLogo}></Image>
           <h3 className="navbar__title">UPBOX</h3>
+          <a className="navbar__menu-button" onClick={toggleMenu}>
+            <IoMdMenu />
+          </a>
         </div>
         <div className="navbar__right-side">
           <h3>MAKEUP</h3>
@@ -19,8 +29,13 @@ export default function ContactNav() {
           <h3>SALE</h3>
           <button>LOG IN &gt;</button>
         </div>
-        <div className="navbar__menu">
-          <IoMdMenu />
+        <div className={`navbar__menu ${isOpen ? 'open' : ''}`}>
+          <ul>
+            <li>MAKEUP</li>
+            <li>BATH</li>
+            <li>SALE</li>
+            <button>LOG IN &gt;</button>
+          </ul>
         </div>
       </div>
     </div>
--- src/components/upBox/navbar.js
+++ src/components/upBox/navbar.js
@@ -1,10 +1,17 @@
-import React from "react";
+'use client';
 import Image from 'next/image';
 import upboxLogo from '/public/images/upboxLogo.svg';
 import topbar from '/public/images/topbar.svg';
 import { IoMdMenu } from "react-icons/io";
+import React, {useState} from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="navbar">
       <Image className="navbar__topbar" src={topbar}></Image>
@@ -12,6 +19,9 @@ export default function Navbar() {
         <div className="navbar__logoGroup">
           <Image className="navbar__logo" src={upboxLogo}></Image>
           <h3 className="navbar__title">UPBOX</h3>
+          <a className="navbar__menu-button" onClick={toggleMenu}>
+            <IoMdMenu />
+          </a>
         </div>
         <div className="navbar__right-side">
           <h3>THIS MONTH</h3>
@@ -21,8 +31,15 @@ export default function Navbar() {
           <h3>SALE</h3>
           <button>LOG IN &gt;</button>
         </div>
-        <div className="navbar__menu">
-          <IoMdMenu />
+        <div className={`navbar__menu ${isOpen ? 'open' : ''}`}>
+          <ul>
+            <li>THIS MONTH</li>
+            <li>SKIN</li>
+            <li>HAIR</li>
+            <li>BATH</li>
+            <li>SALE</li>
+            <button>LOG IN &gt;</button>
+          </ul>
         </div>
       </div>
     </div>
